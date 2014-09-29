19c7a919a4e80dca5b00358567050863a3ab6d37
xuwei-k <6b656e6a69@gmail.com>
2014-09-29 21:11:52 +0900

    default action2html impl

--- config.scala
+++ config.scala
@@ -13,5 +13,36 @@ new Config{
     val accessToken       = ""
     val accessTokenSecret = ""
   }
+
+  def action2html(action: UserAction): String = {
+    val getLine = action.content.lines.toArray.lift
+    action.content.lines.zipWithIndex.map{
+      case (line, index)
+        if line.size > 50 &&
+           getLine(index - 1).exists(_.contains("<blockquote>")) &&
+           getLine(index + 1).exists(_.contains("</blockquote>")) &&
+           (false == line.contains("<a href=")) &&
+           (false == line.contains("<img "))
+         =>
+
+        val words = line.split(' ').iterator
+        var lines: List[String] = Nil
+        var current = new StringBuilder(" ")
+        while (words.hasNext) {
+          if (current.size > 40) {
+            lines ::= current.result()
+            current = new StringBuilder(words.next() + " ")
+          } else {
+            current.append(words.next())
+            current.append(" ")
+          }
+        }
+        lines ::= current.result()
+        lines.reverseIterator.mkString("<br />")
+      case (line, _) =>
+        line
+    }.mkString("\n")
+  }
+
 }
 
