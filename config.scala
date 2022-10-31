1cb2e813bc56fc18cdea3fc0a689e24e7357301d
Scala Steward <2517319+scala-steward-bot@users.noreply.github.com>
2022-10-31 23:19:24 +0000

    Reformat with scalafmt 3.6.1
    Executed command: scalafmt --non-interactive

--- config.scala
+++ config.scala
@@ -2,26 +2,33 @@ import java.net.URL
 import githubot._
 import scala.concurrent.duration._
 
-new Config{
+new Config {
   val rss = new URL("")
   val interval = 3.minute
   override val filter = { (u: UserAction) =>
     val s = u.tweetString
-    List("codecov-io", "googlebot", "asfgit", "grpc-jenkins", "netkins", "buildhive", "scala-jenkins", "akka-ci", "asfbot", "ConfluentJenkins").forall(disable => ! s.contains(disable))
+    List("codecov-io", "googlebot", "asfgit", "grpc-jenkins", "netkins", "buildhive", "scala-jenkins", "akka-ci", "asfbot", "ConfluentJenkins")
+      .forall(disable => !s.contains(disable))
   }
-  val twitter = new TwitterSettings{
-    val consumerKey       = ""
-    val consumerSecret    = ""
-    val accessToken       = ""
+  val twitter = new TwitterSettings {
+    val consumerKey = ""
+    val consumerSecret = ""
+    val accessToken = ""
     val accessTokenSecret = ""
   }
 
   private[this] val shouldNotAddImageWords = Set(
-    "starred", "forked", "created branch", "created repository", "deleted branch", "created tag", "deleted tag"
+    "starred",
+    "forked",
+    "created branch",
+    "created repository",
+    "deleted branch",
+    "created tag",
+    "deleted tag"
   ).map(" " + _ + " ")
 
   override val addImage = { (action: UserAction) =>
-    ! shouldNotAddImageWords.exists(word => action.title.contains(word))
+    !shouldNotAddImageWords.exists(word => action.title.contains(word))
   }
 
   private def removeHtmlTags(string: String): Option[String] =
@@ -34,9 +41,11 @@ new Config{
 
   private def splitLine(str: String): String = {
     val delimiters = List(' ', '。', '、', '　', ' ')
-    val words: Iterator[String] = delimiters.foldLeft(Array(str.trim)){ 
-      (strings, c) => strings.flatMap(_.split(c))
-    }.iterator
+    val words: Iterator[String] = delimiters
+      .foldLeft(Array(str.trim)) { (strings, c) =>
+        strings.flatMap(_.split(c))
+      }
+      .iterator
     var lines: List[String] = Nil
     var current = new StringBuilder(" ")
     while (words.hasNext) {
@@ -64,20 +73,17 @@ new Config{
       (getLine(index - 2).exists(_ == div))
     }
 
-    action.content.lines.zipWithIndex.filterNot{
-      case (line, index) =>
-        isIgnoreDivElem(line, index, titleClassDiv) || isIgnoreDivElem(line, index, timeClassDiv)
-    }.map{
+    action.content.lines.zipWithIndex.filterNot { case (line, index) =>
+      isIgnoreDivElem(line, index, titleClassDiv) || isIgnoreDivElem(line, index, timeClassDiv)
+    }.map {
       case (line, index)
-        if line.trim.size > 80 &&
-           (getLine(index - 1).exists(_.contains("<blockquote>")) || getLine(index + 1).exists(_.contains("</blockquote>")))
-         =>
-
+          if line.trim.size > 80 &&
+            (getLine(index - 1).exists(_.contains("<blockquote>")) || getLine(index + 1).exists(_.contains("</blockquote>"))) =>
         removeHtmlTags(line) match {
           case Some(str) =>
             splitLine(str)
           case None =>
-            if(line.contains("<a href=") || line.contains("<img ")){
+            if (line.contains("<a href=") || line.contains("<img ")) {
               line
             } else {
               splitLine(line)
